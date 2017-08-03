c915e35882de18771a191f6387837444db4e8e8d
Adam Urban <adam.karol.urban@gmail.com>
2017-08-03 22:33:12 +0200

    Removed unused dependency

--- front/app.module.js
+++ front/app.module.js
@@ -4,7 +4,6 @@ import satellizer from 'satellizer'
 import routing from './app.routing';
 import auth from './app.auth';
 import uirouter from '@uirouter/angularjs';
-import 'angular-google-chart';
 
 import common from './common';
 import login from './login';
@@ -12,7 +11,6 @@ import home from './home';
 
 const photostoryApp = angular.module('photostoryApp', [
   uirouter,
-  'googlechart',
   satellizer,
   login,
   common,
